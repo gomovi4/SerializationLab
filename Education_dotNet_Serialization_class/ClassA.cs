88441cfc11b7558f2d289634e10fbbda5e8e315b
gomovi4 <gomovi4@gmail.com>
2020-02-25 08:29:23 +0200

    Serialization Lab

---
+++ Education_dotNet_Serialization_class/ClassA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Education_dotNet_Serialization_class
+{
+
+    [Serializable]
+    //public class Order: IDeserializationCallback
+    public class Order
+    {
+        public int Count;
+
+        public decimal Price;
+        [NonSerialized]
+        public decimal TotalPrice;
+
+
+        public Order() { }
+        public Order(int count, decimal price)
+        {
+            this.Count = count;
+            this.Price = price;
+            this.TotalPrice = count * price;
+        }
+
+        /*public void OnDeserialization(object sender) {
+            TotalPrice = Count * Price;
+        }*/
+
+        [OnDeserialized]
+        private void onDeserialized(StreamingContext context) {
+            TotalPrice = Count * Price;
+        }
+    }
+}
---
+++ Education_dotNet_Serialization_class/Serialization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace Education_dotNet_Serialization_class
+{
+    class Serialization   
+    {
+       
+        static void Main(String[] args) {
+
+            Order order = new Order(4,5.5m);
+
+            //BinaryFormatter
+
+            /*FileStream fsc = new FileStream("d:\\testSR1.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();            
+            binaryFormatter.Serialize(fsc, order);
+            fsc.Close();
+
+            FileStream fsr = File.OpenRead("d:\\testSR1.bin");
+            BinaryFormatter binaryFormatter1 = new BinaryFormatter();
+            Order neworder = (Order)binaryFormatter1.Deserialize(fsr);
+            fsr.Close();
+
+           Console.WriteLine("Count: {0}",neworder.Count);
+           Console.WriteLine("Price: {0}", neworder.Price);
+           Console.WriteLine("Total Price: {0}", neworder.TotalPrice);
+           Console.ReadKey();*/
+
+            //XMLSerializer
+
+            FileStream fsc = new FileStream("d:\\testSR.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+            XmlSerializer xml = new XmlSerializer(typeof(Order));
+            xml.Serialize(fsc, order);
+            fsc.Close();
+
+            FileStream fsr = File.OpenRead("d:\\testSR.xml");
+            XmlSerializer xml1 = new XmlSerializer(typeof(Order));
+            xml.Deserialize(fsr);
+            fsr.Close();
+
+            Console.WriteLine("Count: {0}",order.Count);
+            Console.WriteLine("Price: {0}", order.Price);
+            Console.WriteLine("Total Price: {0}", order.TotalPrice);
+            Console.ReadKey();
+
+           
+        }
+
+    }
+}
