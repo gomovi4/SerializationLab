4fffff977551cf71f2ca520e4ab89c5e26ab363f
gomovi4 <gomovi4@gmail.com>
2020-07-21 18:59:58 +0300

    added new implementation of task for delegates

---
+++ WindowsFormsAppforDelegates/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppforDelegates
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextBoxValue(string data);
+        SetTextBoxValue setTextBoxValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 F2 = new Form2();
+            setTextBoxValue += F2.SetMessage;
+            F2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setTextBoxValue(textBox1.Text);
+        }
+    }
+}
