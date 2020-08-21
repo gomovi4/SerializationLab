d46149fa1279a1e5ddce6f9af6aee77ff22926cf
gomovi4 <gomovi4@gmail.com>
2020-08-21 14:06:20 +0300

    test task for Threads

---
+++ Spellchecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace Spellchecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Spellchecker spellchecker = new Spellchecker();
+            spellchecker.LaunchUtility();
+           Thread th1 = new Thread(spellchecker.ReadFile);
+           th1.Start();
+           th1.Join(1200);
+            
+           Thread th2 = new Thread(spellchecker.CheckSpelling);
+           th2.Start();
+           th2.Join(1000);
+
+            }
+        
+    }
+}
